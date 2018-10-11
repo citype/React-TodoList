60387b64e086ef01fdc838d3ef0ab35ead340e38
xihe <xihe@2dfire.com>
2018-10-11 09:09:45 +0800

    实现 store 中数据与组件中的input框联动

--- todo-list-demo/src/store/reducer.js
+++ todo-list-demo/src/store/reducer.js
@@ -3,6 +3,14 @@ const defaultState = {
     list: [1,2]
 }
 
+// reducer 可以接收 state 但是绝不能修改 state
 export default (state = defaultState, action) => {
+    if(action.type === 'change_input_value') {
+        // 深拷贝
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+
+    }
     return state;
 }
