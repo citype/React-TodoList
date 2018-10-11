c9215cbc24ec3221254487fc6ce5fcbfef9c7aad
xihe <xihe@2dfire.com>
2018-10-11 09:18:41 +0800

    实现store inputvalue 改变点击提交之后更新 store中list数据

--- todo-list-demo/src/TodoList.js
+++ todo-list-demo/src/TodoList.js
@@ -8,51 +8,61 @@ import { List } from 'antd';
 import store from './store'
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        console.log(store.getState())
-        this.state = store.getState()
-        this.handleInputChange = this.handleInputChange.bind(this)
-        this.handleStoreChange = this.handleStoreChange.bind(this)
-        // 让组件里面的数据跟着 store 更新 订阅 store 只要 store 发生改变 handleStoreChange 将被调用
-        store.subscribe(this.handleStoreChange);
-    }
-    
-    render() {
-        return(
-        
-            <div style={{marginTop:'10px',marginLeft:'10px'}}>
-                
-                <Input value = {this.state.inputValue} 
-                placeholder = 'todo info' 
-                style={{width:'300px',marginRight:'10px'}}
-                onChange={this.handleInputChange}
-                />
-                
-                <Button type="primary">提交</Button>
-                
-                <List style={{marginTop:'10px',width:'300px'}}
-                dataSource={this.state.list}
-                renderItem={item => (<List.Item>{item}</List.Item>)}
-                />
-            </div>
-        )
-    }
-    handleInputChange(e) {
-        // 告诉 sotre 改变 input—value 的值为，e.target.value
-        const action = {
-            type : 'change_input_value',
-            value: e.target.value
-        }
-        store.dispatch(action)
-        console.log(e.target.value)
-    }
-    handleStoreChange() {
-        console.log("sotre changed")
-        // 从 store 重新取数据
-        // 替换掉当前的数据
-        this.setState(store.getState());
-    }
- }
+  constructor(props) {
+    super(props);
+    console.log(store.getState());
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    // 让组件里面的数据跟着 store 更新 订阅 store 只要 store 发生改变 handleStoreChange 将被调用
+    store.subscribe(this.handleStoreChange);
+  }
+
+  render() {
+    return (
+      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+        <Input
+          value={this.state.inputValue}
+          placeholder="todo info"
+          style={{ width: "300px", marginRight: "10px" }}
+          onChange={this.handleInputChange}
+        />
+
+        <Button type="primary" onClick={this.handleButtonClick}>
+          提交
+        </Button>
+
+        <List
+          style={{ marginTop: "10px", width: "300px" }}
+          dataSource={this.state.list}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+      </div>
+    );
+  }
+  handleInputChange(e) {
+    // 告诉 sotre 改变 input—value 的值为，e.target.value
+    const action = {
+      type: "change_input_value",
+      value: e.target.value
+    };
+    store.dispatch(action);
+    console.log(e.target.value);
+  }
+  handleStoreChange() {
+    console.log("sotre changed");
+    // 从 store 重新取数据
+    // 替换掉当前的数据
+    this.setState(store.getState());
+  }
+  handleButtonClick() {
+      const action = {
+          type :"add_todo_item",
+      }
+      store.dispatch(action)
+
+  }
+}
 
  export default TodoList;
--- todo-list-demo/src/store/reducer.js
+++ todo-list-demo/src/store/reducer.js
@@ -1,16 +1,24 @@
 const defaultState = {
-    inputValue: '123',
-    list: [1,2]
+    inputValue: '',
+    list: []
 }
 
 // reducer 可以接收 state 但是绝不能修改 state
 export default (state = defaultState, action) => {
+
     if(action.type === 'change_input_value') {
         // 深拷贝
         const newState = JSON.parse(JSON.stringify(state));
         newState.inputValue = action.value;
         return newState;
-
     }
+
+    if (action.type === "add_todo_item") {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = "";
+        return newState;
+    } 
+
     return state;
 }
