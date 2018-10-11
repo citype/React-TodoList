34b8dd9720eb817d6b3cba168dc625124d1186d8
xihe <xihe@2dfire.com>
2018-10-11 08:43:07 +0800

    增加Redux 调试工具

--- todo-list-demo/src/store/index.js
+++ todo-list-demo/src/store/index.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux'
 import reducer from './reducer.js'
 
-const store = createStore(reducer)
+const store = createStore(reducer,
+    // 如果页面有安装这个工具，则使用
+    window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+
 
 export default store;
